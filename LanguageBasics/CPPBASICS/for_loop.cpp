25cb3be9e748fbd0b497389cb7a7a7307161a5cb
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-06-30 07:03:06 +0530

    30-06-26

---
+++ LanguageBasics/CPPBASICS/for_loop.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int num;
+    // for(int i=1; i<=2; i++){
+    //     cin>>num;
+    //     cout<<num<<endl;
+    // }
+
+    // for(int i=1; i++){
+    //     cout<<i;
+    // }
+
+    // for(int i=1; i<10; i++){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=10; i>=1; i--){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=10; i>=1; i=i-2){
+    //     cout<<i<<endl;
+    // }
+
+    for(int i=5; i<=100; i=i+5){
+        cout<<i<<endl;
+    }
+    return 0;
+}
