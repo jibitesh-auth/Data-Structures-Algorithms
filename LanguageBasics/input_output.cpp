026b8af4d15684202ca064b401edfcf47968ede5
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-01 12:57:34 +0530

    01-07-26

---
+++ LanguageBasics/input_output.cpp
@@ -0,0 +1,41 @@
+/*Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+
+Use:-
+
+for C++ : cout << variable_name;
+for Java : System.out.print();
+for Python : print()
+for Javascript : console.log()
+
+Example 1
+
+Input(user gives value): 7
+
+Output: 7
+
+Example 2
+
+Input(user gives value): -5
+
+Output: -5*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+   void printNumber(){
+     int num;
+     cin >> num;
+     cout << num;
+   }
+
+};
+
+// int main(){
+//     Solution s;
+//     s.printNumber();
+//     return 0;
+// }
