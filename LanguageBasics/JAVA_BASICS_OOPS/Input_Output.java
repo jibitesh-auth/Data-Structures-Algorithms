e5d75c286684e4e7bfe0ca2a04a581589a4d095e
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-06 07:37:56 +0530

    06-07-26

---
+++ LanguageBasics/JAVA_BASICS_OOPS/Input_Output.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Input_Output{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // int num = scanner.nextInt();
+        double num = scanner.nextDouble();
+        System.out.println(num);
+        
+        scanner.close();
+
+    }
+}
--- LanguageBasics/JAVA_BASICS_OOPS/String1.java
+++ LanguageBasics/JAVA_BASICS_OOPS/String1.java
@@ -7,7 +7,19 @@ class String1{
 
         // System.out.println(name.substring(0,4));
 
-        System.out.println(name.substring(0,5));
+        // System.out.println(name.substring(0,5));
+
+        String lastName = "Mishra";
+        // String fullName = name + " " + lastName;
+        // System.out.println(fullName);
+
+        // name += " " + lastName;
+        // System.out.println(name);
+
+        // boolean result = name.equals(lastName);
+
+        boolean result = name == lastName;
+        System.out.println(result);
 
 
 
