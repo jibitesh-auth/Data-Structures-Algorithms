9b50e45db7b9cb6f0095194ec2efc6e93b811271
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-08-18 21:49:57 +0530

    18-08-26

--- Basic Maths/LCM.cpp
+++ Basic Maths/LCM.cpp
@@ -1,29 +1,69 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         int LCM(int n1, int n2){
+//             int i = 1;
+//             int max1 = max(n1,n2);
+//             do{
+//                 int multiple = i * max1;
+//                 if(multiple % n1 == 0 && multiple % n2 == 0){
+//                     return multiple;
+//                 }
+//                 i = i+1;
+//             }while(1);
+
+
+//         }
+// };
+
+// int main(){
+//     int n1, n2;
+//     cin >> n1 >> n2;
+//     Solution s;
+//     cout << s.LCM(n1,n2);
+//     return 0;
+
+
+// }
+
+//*T.C: O(min(n1,n2)) 
+//min(n1,n2) : gives no of iteration(operation)
+//*S.C: O(1)
+
+
+//*OR
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution{
-    public:
-        int LCM(int n1, int n2){
-            int i = 1;
-            int max1 = max(n1,n2);
-            do{
-                int multiple = i * max1;
-                if(multiple % n1 == 0 && multiple % n2 == 0){
-                    return multiple;
-                }
-                i = i+1;
-            }while(1);
-
-
+    private:
+        int GCD(int n1, int n2){
+            while(n2 != 0){
+                int r = n1 % n2;
+                n1 = n2;
+                n2 = r;
+            }
+            return n1;
         }
+    public:
+        int LCM(int n1,int n2){
+            return (n1*n2)/GCD(n1,n2);
+        }
+
 };
 
 int main(){
-    int n1, n2;
+    int n1,n2;
     cin >> n1 >> n2;
     Solution s;
     cout << s.LCM(n1,n2);
     return 0;
-
-
 }
+
+//*T.C: O(log(min(n1,n2)))
+//*S.C: O(1)
+
+
+
---
+++ Basic Maths/divisor.cpp
@@ -0,0 +1,113 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         vector<int> divisors(int n){
+//             vector<int> vec;
+//             for(int i=1; i<=n; i++){
+//                 if(n%i == 0){
+//                     vec.push_back(i);
+                    
+//                 }
+//             }
+//             return vec;
+//         }
+// };
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     Solution s;
+//     for(auto at: s.divisors(n)){
+//         cout << at << " ";
+//     }
+//     return 0;
+// }
+
+//*T.C: O(n)
+//*S.C: O(K) -----> not more than sqrt(N)
+
+//------------------x--------------
+//*OR
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         vector<int> divisors(int n){
+//             vector<int> vec;
+//             for(int i=1; i*i<=n; i++){
+//                 if(n%i == 0){
+//                     vec.push_back(i);
+//                     if(i != n/i){
+//                         vec.push_back(n/i);
+//                     }
+                    
+//                 }
+//             }
+//             sort(vec.begin(),vec.end());  //*O(NlogN) -> T.C
+//             return vec;
+//         }
+// };
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     Solution s;
+//     for(auto at: s.divisors(n)){
+//         cout << at << " ";
+//     }
+//     return 0;
+// }
+
+//*T.C: O(sqr(n)) + O(Klogn):(sort)
+//*S.C: O(K)  -------> max it can be O(sqrt(N))
+
+//--------------x---------------
+
+//*OR
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        vector<int> divisors(int n){
+            vector<int> vec;
+            vector<int> vec1;
+            for(int i=1; i*i<=n; i++){
+                if(n%i == 0){
+                    vec.push_back(i);
+                    if(i != n/i){
+                        vec1.push_back(n/i);
+                    }
+                    
+                }
+            }
+            for(auto i= vec1.rbegin(); i<vec1.rend(); i++){
+                vec.push_back(*i);
+            }
+            return vec;
+        }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    Solution s;
+    for(auto at: s.divisors(n)){
+        cout << at << " ";
+    }
+    return 0;
+}
+
+//*T.C: O(sqrt(N))
+//*S.C: O(K)
+
+
+//*Note: use rbegin(), rend() instead of begin(), end() in reverse ( because end() -1 for empty will give runtime error)
+
+
+
