9448a7264d0f3b8beead61387a5aa0c9f7ea9c00
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-08-31 17:12:10 +0530

    31-08-26

--- Concept Basics/Basic Hashing/second_highest_occuring_element.cpp
+++ Concept Basics/Basic Hashing/second_highest_occuring_element.cpp
@@ -1,64 +1,123 @@
-#include <bits/stdc++.h>
-using namespace std;
+// OPTIMAL
+//  #include <bits/stdc++.h>
+//  using namespace std;
 
-class Solution{
-    public:
-        int secondMostFrequentElement(vector<int> &nums){
-            int n = nums.size();
-            unordered_map<int,int> mpp;
-            int maxcnt = 0,secondcnt = 0, el1 = -1, el2=-1;
-            for(int i=0; i<n; i++){
-                mpp[nums[i]]++;
+// class Solution{
+//     public:
+//         int secondMostFrequentElement(vector<int> &nums){
+//             int n = nums.size();
+//             unordered_map<int,int> mpp;
+//             int maxcnt = 0,secondcnt = 0, el1 = -1, el2=-1;
+//             for(int i=0; i<n; i++){
+//                 mpp[nums[i]]++;
 
-            }
-            for(auto it: mpp){
-                int element = it.first;
-                int freq = it.second;
-                if(freq > maxcnt){
-                    el2 = el1;
-                    secondcnt = maxcnt;
-                    maxcnt = freq;
-                    el1 = element;
-                }
-                else if(freq == maxcnt){
-                   
-                    el1 = min(element,el1);
-                }
-                else if(freq > secondcnt){
-                    secondcnt = freq;
-                    el2 = element;
+//             }
+//             for(auto it: mpp){
+//                 int element = it.first;
+//                 int freq = it.second;
+//                 if(freq > maxcnt){
+//                     el2 = el1;
+//                     secondcnt = maxcnt;
+//                     maxcnt = freq;
+//                     el1 = element;
+//                 }
+//                 else if(freq == maxcnt){
 
-                }
-                else if(freq == secondcnt){
-                    el2 = min(element,el2);
-                }
-                
+//                     el1 = min(element,el1);
+//                 }
+//                 else if(freq > secondcnt){
+//                     secondcnt = freq;
+//                     el2 = element;
 
-            }
-            return el2;
+//                 }
+//                 else if(freq == secondcnt){
+//                     el2 = min(element,el2);
+//                 }
 
-        }
-};
+//             }
+//             return el2;
 
-int main(){
-    int n,x;
-    cin >> n;
-    vector <int> nums;
-    for(int i=0; i<n; i++){
-         cin >> x;
-         nums.push_back(x);
-    }
-    Solution s;
-    cout<< s.secondMostFrequentElement(nums);
+//         }
+// };
 
-    return 0;
+// int main(){
+//     int n,x;
+//     cin >> n;
+//     vector <int> nums;
+//     for(int i=0; i<n; i++){
+//          cin >> x;
+//          nums.push_back(x);
+//     }
+//     Solution s;
+//     cout<< s.secondMostFrequentElement(nums);
 
-    
-}
+//     return 0;
+
+// }
 
 //*T.C: O(N)
 //*S.C: O(N)
-
-
 //--------------x-----------------
 
+// BRUTE FORCE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int secondMostFrequentElement(vector<int> &vec)
+    {
+        int n = vec.size();
+        int maxFreq = 0, secMaxFreq = 0;
+
+        int maxEle = -1, secEle = -1;
+
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i])
+                continue;
+
+            int freq = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (vec[i] == vec[j])
+                {
+                    freq++;
+                    visited[j] = true;
+                }
+            }
+            if (freq > maxFreq)
+            {
+                secMaxFreq = maxFreq;
+                maxFreq = freq;
+                secEle = maxEle;
+                maxEle = vec[i];
+            }
+            else if (freq == maxFreq)
+            {
+                maxEle = min(vec[i], maxEle);
+            }
+            else if (freq > secMaxFreq)
+            {
+                secMaxFreq = freq;
+                secEle = vec[i];
+            }
+            else if (freq == secMaxFreq)
+            {
+                secEle = min(secEle, vec[i]);
+            }
+        }
+        return secEle;
+    }
+};
+
+int main()
+{
+    vector<int> vec = {4, 4, 5, 5, 6, 7};
+    Solution s;
+    cout << s.secondMostFrequentElement(vec);
+}
---
+++ Concept Basics/Basic Hashing/sum_highest_lowest.cpp
@@ -0,0 +1,160 @@
+
+//*OPTIMAL
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         int mostFrequentElement(vector<int> &nums){
+//             unordered_map<int,int> mpp;
+//             for(auto it:nums){
+//                 mpp[it]++;
+//             }
+//             int maxcnt = 0;
+
+//             for(auto it: mpp){
+//                 int ele = it.first;
+//                 int freq = it.second;
+
+//                 if(freq > maxcnt){
+//                     maxcnt = freq;
+//                 }
+
+//             }
+//             return maxcnt;
+
+//         }
+//         int minFrequentElement(vector<int> &nums){
+//             unordered_map<int,int> mpp;
+//             int n = nums.size();
+//             for(auto it:nums){
+//                 mpp[it]++;
+//             }
+//             int mincnt = n;
+
+//             for(auto it: mpp){
+//                 int ele = it.first;
+//                 int freq = it.second;
+
+//                 if(freq < mincnt){
+//                     mincnt = freq;
+//                 }
+
+//             }
+//             return mincnt;
+
+//         }
+        
+// };
+
+// int main(){
+//     int n,x;
+//     cin >> n;
+//     vector <int> nums;
+//     for(int i=0; i<n; i++){
+//          cin >> x;
+//          nums.push_back(x);
+//     }
+//     Solution s1;
+//     int a = s1.mostFrequentElement(nums);
+//     int b = s1.minFrequentElement(nums);
+//     cout << a+b;
+
+//     return 0;
+
+    
+// }
+//*T.C: O(N)
+//*S.C: O(N)
+//-------------------------------------------X--------------------------------------------------------------
+//*Brute Force
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         int sum_highest_lowest(vector<int> vec){
+//             int n = vec.size();
+//             vector<bool> vis(n,false);
+//             int maxFreq = 0, minFreq = n;
+//             for(int i=0; i<n; i++){
+//                 if(vis[i]){
+//                     continue;
+//                 }
+//                 int Freq = 0;
+//                 for(int j=0; j<n; j++){
+//                     if(vec[i] == vec[j]){
+//                         Freq++;
+//                         vis[j] = true;
+//                     }
+
+//                 }
+//                 maxFreq = max(Freq,maxFreq);
+//                 minFreq = min(Freq,minFreq);
+
+
+
+//             }
+//             return minFreq+maxFreq;
+//         }
+// };
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> vec;
+//     int x;
+//     for(int i=0; i<n; i++){
+//         cin >> x;
+//         vec.push_back(x);
+
+//     }
+
+//     Solution s;
+//     cout << s.sum_highest_lowest(vec);
+//     return 0;
+// }
+
+//*T.C: O(N^2)
+//*S.C: O(N)
+
+//-----------------------------x--------------------------------------------
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+// public:
+//      int sumHighestAndLowestFrequency(vector<int> &nums){
+//         int n=nums.size();
+//         int maxFreq=0;
+//         int minFreq=n;
+//         unordered_map<int,int> mpp;
+//         for(int i=0;i<n;i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(auto it:mpp){
+//             int freq=it.second;
+//             if(freq>maxFreq){
+//                 maxFreq=freq;
+//             }
+//             if(freq<minFreq){
+//                 minFreq=freq;
+//             }
+//         }
+//         return maxFreq+minFreq;
+//      }
+// };
+
+// int main(){
+//     vector<int> nums={4,4,5,5,6};
+
+//     Solution sol;
+//     int ans=sol.sumHighestAndLowestFrequency(nums);
+//     cout<<ans;
+//     return 0;
+
+// }
+
+//*T.C: O(N)
+//*S.C: O(N)
