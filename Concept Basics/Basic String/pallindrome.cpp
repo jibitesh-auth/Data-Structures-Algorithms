7d5a7f1e58da7e2827f7dbe5efdc820deb83e1ab
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-09-02 19:55:08 +0530

    02-09-26

--- Concept Basics/Basic String/pallindrome.cpp
+++ Concept Basics/Basic String/pallindrome.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-class Solution{
-    public:
-        bool pallindrome(string s){
-            int n = s.size();
+// class Solution{
+//     public:
+//         bool pallindrome(string& s){
+//             int n = s.size();
             // int start = 0, end = s.size() -1;
             // while(start < end){
             //     if(s[start] != s[end]){
@@ -14,17 +14,56 @@ class Solution{
             //     end--;
             // }
 
-            //* OR
+             //* OR
 
+//             for(int i=0; i<n/2; i++){
+//                 if(s[i] != s[n-i-1]){
+//                     return false;
+//                 }
+//             }
+//             return true;
+
+
+//         }
+// };
+
+// int main(){
+//     string s;
+//     cin >> s;
+
+//     Solution s2;
+//     bool ans = s2.pallindrome(s);
+//     if(ans){
+//         cout << "Pallindrome";
+//     }
+//     else{
+//         cout << "Not Pallindrome";
+//     }
+
+    
+
+//     return 0;
+// }
+
+//*T.C: O(N)
+//*S.C: O(1)
+
+//* Can also be done using recursion
+
+//--------------------------------X------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        bool pallindrome(string& s){
+            string s1 = s;
+            int n = s.size();
             for(int i=0; i<n/2; i++){
-                if(s[i] != s[n-i-1]){
-                    return false;
-                }
-
+                swap(s[i],s[n-i-1]);
             }
-            return true;
 
-
+            return s1==s;
         }
 };
 
@@ -46,7 +85,7 @@ int main(){
     return 0;
 }
 
-//*T.C: O(N)
+//*T.C: O(n)
 //*S.C: O(1)
 
-//* Can also be done using recursion
+
